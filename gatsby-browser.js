2dd6e3b56c53d31b201402ecccab4abb5ff3e927
D-Astudillo-ASC <dma3@williams.edu>
2025-05-08 16:25:22 -0400

    Testing font fixes in Vercel.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,8 @@
-import "@fontsource/playfair-display"
+// Import Playfair Display font with specific weights
+import "@fontsource/playfair-display/400.css"  // normal
+import "@fontsource/playfair-display/500.css"  // medium
+import "@fontsource/playfair-display/600.css"  // semibold
+import "@fontsource/playfair-display/700.css"  // bold
 
 import React from "react"
 import Layout from "./src/components/layout.js"
