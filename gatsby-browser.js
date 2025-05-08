ceb763a1990fe5b4c8aa759268e53a84045eae6e
D-Astudillo-ASC <dma3@williams.edu>
2025-05-08 16:32:33 -0400

    More Vercel font fixes.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,8 +1,16 @@
 // Import Playfair Display font with specific weights
-import "@fontsource/playfair-display/400.css"  // normal
-import "@fontsource/playfair-display/500.css"  // medium
-import "@fontsource/playfair-display/600.css"  // semibold
-import "@fontsource/playfair-display/700.css"  // bold
+import "@fontsource/playfair-display/400.css";  // normal
+import "@fontsource/playfair-display/400-italic.css";
+import "@fontsource/playfair-display/500.css";  // medium
+import "@fontsource/playfair-display/500-italic.css";
+import "@fontsource/playfair-display/600.css";  // semibold
+import "@fontsource/playfair-display/600-italic.css";
+import "@fontsource/playfair-display/700.css";  // bold
+import "@fontsource/playfair-display/700-italic.css";
+import "@fontsource/playfair-display/800.css";  // extra-bold
+import "@fontsource/playfair-display/800-italic.css";
+import "@fontsource/playfair-display/900.css";  // black
+import "@fontsource/playfair-display/900-italic.css";
 
 import React from "react"
 import Layout from "./src/components/layout.js"
